3a00e757422b5bc1f290a40e008bdc60736957a5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-13 21:46:35 +0530

    Added the scripts for displaying words...

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ const playAgainBtn = document.getElementById("play-again");
 const popup = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = [
+  "Programming",
+  "Application",
+  "Wizard",
+  "Nerd",
+  "Development",
+  "Design",
+];
+
+const selectedWords = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function displayWords() {
+  wordEl.innerHTML = `
+    ${selectedWords
+      .split("")
+      .map(
+        (letter) => `
+          <span class="letter"> ${
+            correctLetters.includes(letter) ? letter : ""
+          } </span>
+        `
+      )
+      .join("")}
+  `;
+}
+
+displayWords();
