d96162bffe8f3b6c48fbb44e412344301a16a19a
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-15 12:46:14 +0530

    Added the scripts for the listening the keyboard event...

--- script.js
+++ script.js
@@ -42,4 +42,28 @@ function displayWords() {
   }
 }
 
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWords();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
 displayWords();
